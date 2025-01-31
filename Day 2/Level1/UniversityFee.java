dac3fb4da447d583e4abb46d35250b75c4339c64
ayush071203 <ayushshrivastav0720@gmail.com>
2025-01-31 11:08:16 +0530

    Added

--- Day 2/Level1/UniversityFee.java
+++
@@ -1,8 +0,0 @@
-public class UniversityFee {
-    public static void main(String[] args) {
-        int fee = 125000;
-        int discountPercent = 10;
-        int discount = (fee * discountPercent) / 100;
-        System.out.println("The discount amount is INR " + discount + " and final discounted fee is INR " + (fee - discount));
-    }
-}
